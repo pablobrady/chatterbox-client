7c9466253f6705495d3cd40e2452590ee0d9d8c3
MKS-Student <mks.student@hackreactor.com>
2015-03-17 17:10:22 -0700

    Friends feature working.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,5 @@
+var lastGetMsgTime = "";
+
 var app = {
 	username: '<anonymous>',
 	server: 'https://api.parse.com/1/classes/chatterbox',
@@ -5,7 +7,8 @@ var app = {
 	init: function() {
 		//initialize function to start timers, get initial messages
 		// fix:  $('#username').text(app.username);
-		getMessages();
+		app.fetch();
+		setInterval(app.fetch, 3000);
 	},
 	send: function(message) {
 		// Send an AJAX Post request.
@@ -13,11 +16,25 @@ var app = {
 	},
 	fetch: function() {
 		// Send an AJAX get request.
-		getMessages();
+
+		var dataConstraints = {};
+		dataConstraints['order'] = '-createdAt';
+		if (lastGetMsgTime) {
+			dataConstraints['where'] = {'updatedAt': {'$gt': lastGetMsgTime}};
+		};
+		console.log(dataConstraints);
+	  //  // {'roomname': 'lobby', 'username': 'matt_david'}
+
+		getMessages(dataConstraints);
 	},
-	clearMessages: function() {
+	clearMessages: function(numToDelete) {
 		// Remove all children from #chats.
-		$('#chats').children().remove();
+		if (numToDelete === undefined){
+			numToDelete = $('#chats').children().length;
+		}
+		for (var i = 0; i < numToDelete; i++){
+		  $('#chats').children(':first').remove();
+		}
 	},
 	addMessage: function(message) {
 		//append HTML element to chats
@@ -58,7 +75,8 @@ var postMessage = function(message) {
 	    console.log('chatterbox: Message sent');
 	    console.log('post data:');
 	    console.log(data);
-	    getMessages(data);
+	    // getMessages(data);
+	    app.fetch();
 	  	// call update method to update the display
 	  },
 	  error: function (data) {
@@ -68,29 +86,29 @@ var postMessage = function(message) {
 	});
 };
 
-var getMessages = function() {
+var getMessages = function(dataConstraints) {
 	//send AJAX request to get messages from the server
 	  //on success, add to the document message area
 	$.ajax({
 	  // This is the url you should use to communicate with the parse API server.
 	  url: app.server,
 	  type: 'GET', // to create a new resource
-	  data: {
-	  	order: '-createdAt'
-	  	// where: {'roomname': 'lobby', 'username': 'matt_david'}
-	  },
+	  data: dataConstraints,
 	  success: function (data) {
 	    console.log(data);
 	    // Interate over all of data results.
 	    // To prevent a blink, we should cache the messages on screen and only update the newest msgs.
 	      // Store the last time a get request was done.  Only read the message that have appeared since then.
 
+			if (data.results.length > 0)
+				lastGetMsgTime = data.results[0].updatedAt;
+
 	    // to fix: ordering of messages
-	    for(var i=0; i<20; i++) {
-	    	var $message = $('<div><div>');
+	    for(var i=Math.min(data.results.length - 1,20); i>=0; i--) {
+	    	var $message = $('<div class=messageOutput><div>');
 	    	var $username = $('<a href="#"></a>').text(data.results[i].username);
-	    	var $messageText = $('<span class="messageText">' + ': ' + data.results[i].text + '</span>');
-        
+	    	var $messageText = $('<span class="messageText"></span>');
+        $messageText.append(document.createTextNode(data.results[i].text));        
         $username.addClass('user');
 	    	if (app.friendsList[data.results[i].username] === true) {
 	    		$username.addClass('friend'); // if friend, class="user friend"
@@ -100,6 +118,14 @@ var getMessages = function() {
           console.log(this.text);
           app.friendsList[this.text] = true;
           console.log(app.friendsList);
+
+          $('#chats').children().each( function(index, element) {
+          	var curr = $(this).find('a:first');
+          	if( app.friendsList[curr.text()] ) {
+          		curr.addClass("friend");
+          	}
+          });
+
           return false;
         });
 
@@ -109,12 +135,8 @@ var getMessages = function() {
 	    	app.addMessage($message);
 	    }
 
-	    // Build a string that contains the message, username (, time?)
-	    // For each member, call addMessage(text)
-
-
-	  	// call update method to update the display
-	  	// app.addMesages();
+      var toDelete = $('#chats').children().length > 20 ? $('#chats').children().length - 20 : 0;
+      app.clearMessages(toDelete);
 	  },
 	  // error: function (data) {
 	  //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
